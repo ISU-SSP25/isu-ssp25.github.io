a7a8832249e008c14fb86bb44882d70d2d73f78c
Monique Calmon Ferreira Grinstein <moniquecalmon@poli.ufrj.br>
2025-08-19 11:00:38 +0900

    refactor: expand background and rearrange elements

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,18 +1,31 @@
+import { useState, useEffect } from "react";
 import "./HomeComponent.css";
 
 export function HomeComponent({ title, image, text }) {
-    return (
-        <div className="home-component-container">
-            <div className="home-component-text">
-                {title && <h2>Sustainable Lunar Spaceport</h2>}
-                <p>{text}</p> 
-            </div>
-            {image && (
+  const [imageHeight, setImageHeight] = useState(0);
 
-                <div className="home-component-photo">
-                    <img src={image} alt="Project Cover" />
-                </div>
-            )}
-        </div>
-    );
+  useEffect(() => {
+    if (image) {
+      const img = new Image();
+      img.src = image;
+      img.onload = () => setImageHeight(img.height);
+    }
+  }, [image]);
+
+  return (
+    <div
+      className="home-component-container"
+      style={{
+        height: imageHeight ? `${imageHeight}px` : "auto",
+        backgroundImage: image
+          ? `linear-gradient(rgba(255,255,255,0.4), rgba(255,255,255,0.4)), url(${image})`
+          : undefined,
+      }}
+    >
+      <div className="home-component-text">
+        {title && <h2>{title}</h2>}
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 }
