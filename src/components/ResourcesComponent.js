ac92341da1914f2aad76f0ec5308ae2c04cee997
Monique Calmon Ferreira Grinstein <moniquecalmon@poli.ufrj.br>
2025-09-01 21:08:53 +0100

    feat: add embedded presentation video

--- src/components/ResourcesComponent.js
+++ src/components/ResourcesComponent.js
@@ -1,9 +1,10 @@
 import "./ResourcesComponent.css";
 
-export function ResourcesComponent()  {
-  return (
-    <div className="contributors-container">
-      <h2 className="contributors-title">Resources</h2>
-    </div>
-  );
+export function ResourcesComponent() {
+    return (
+        <div className="resources-container">
+            <h2 className="resources-title">Resources</h2>
+            <iframe width="600" height="315" src="https://www.youtube.com/embed/mHIf1K2vR90?si=tFafg1huDd7wafrp&amp;start=19746" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+    );
 }
