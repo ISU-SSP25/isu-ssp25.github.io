d5cb933b3a6fe6f8d34623929f4e6d6d23089aab
Monique Grinstein <moniquecalmon@poli.ufrj.br>
2025-09-05 10:10:59 +0100

    fix: video responsiveness on small screens

--- src/components/ResourcesComponent.js
+++ src/components/ResourcesComponent.js
@@ -4,7 +4,9 @@ export function ResourcesComponent() {
     return (
         <div className="resources-container">
             <h2 className="resources-title">Resources</h2>
-            <iframe width="600" height="315" src="https://www.youtube.com/embed/mHIf1K2vR90?si=tFafg1huDd7wafrp&amp;start=20204" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div className="resources-presentation-video">
+                <iframe width="600" height="315" src="https://www.youtube.com/embed/mHIf1K2vR90?si=tFafg1huDd7wafrp&amp;start=20204" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen />
+        </div>
         </div>
     );
 }
